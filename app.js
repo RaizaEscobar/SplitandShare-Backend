302d2604cc0e9fb719dba6ffe98922e71fe2c376
erifylo <erif.evangelou@gmail.com>
2020-11-17 15:48:40 +0100

    pruba ramaEri

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setup //check
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
