2bc4b403a659aadc7010fcc23737ca3c625e003f
erifylo <erif.evangelou@gmail.com>
2020-11-18 22:19:17 +0100

    favorite flats

--- routes/favorite.js
+++ routes/favorite.js
@@ -3,45 +3,16 @@ const router = express.Router();
 const Flat = require("../models/Flat");
 const User = require('../models/user');
 
-router.post("/flat/:id'", (req, res, next) => {
-   
-    User.findOne({$and:[{'userId' : req.session.currentUser._id },{"flatId": req.params.id}]}).then(user => {
-
-      if (user !== null) {
-  
-        Event.findById(req.body.eventId).then(event=>{
-          Attendee.find({"eventId":req.body.eventId}).populate('userId').exec((err,attendees)=>{
-            const modifiedEvent = {
-              "_id" : event._id,
-              "title": event.title,
-              "date" : dateFormat(event.date,"fullDate" ),
-              "type" : event.type,
-              "city" : event.city,
-              "attendees":attendees,
-              "description":event.description,
-              "image":event.image,
-              "errorMessage" : "already registered!"
-            }
-            console.log(modifiedEvent)
-            res.render('detailEvent', modifiedEvent);
-            return ;
-          }) 
-      
-          });
-      
-      }
-      else{
-        theAttendance.save((err) => {
-          if (err) {
-             next(err);
-            return;
-          }
-        res.redirect('/');
-       });
-      }
-    })
+/* router.post("/flat/:id'", (req, res, next) => {
+    console.log("holiii")
+   User.findByIdAndUpdate(req.session.currentUser, {"favoriteFlats" : req.params.id})
+   .then(() => {
+    res.json({ message: `Flat is updated successfully.` });
+  })
+  .catch(err => {
+    res.json(err);
+  })
     
-   
-  }); 
+  });  */
 
-module.exports = app;
+module.exports = router;
